2c13f4364670f5070e4bb4022e812f3ebf24bc8b
tanyaberezovsky <cokacolata@gmail.com>
2023-12-16 10:58:25 +0200

    Update Transaction.swift

--- MoneyLog/Transaction.swift
+++ MoneyLog/Transaction.swift
@@ -13,6 +13,8 @@ final class Transaction {
     @Attribute(.unique) let id: String
     let timestamp: Date
     let amount: Decimal
+    let type: TransactionTypeString
+    let name: String
     var amountText: String {
         "\(amount)"
     }
@@ -20,6 +22,12 @@ final class Transaction {
         self.timestamp = timestamp
         self.id = UUID().uuidString
         self.amount = amount
+        self.type = TransactionType.none.rawValue
+        self.name = ""
     }
 }
 
+typealias TransactionTypeString = String
+enum TransactionType: String {
+    case none
+}
